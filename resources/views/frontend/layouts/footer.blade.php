17384789fbbc715fd51b64692233fe107885943e
Azhar Raza <walisystems123@gmail.com>
2023-12-15 10:51:09 +0500

    12/15/23 10:51:09 Front footer test change after pull

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,192 +1,187 @@
+<!-- Start Footer Area -->
+<footer class="footer">
+    <!-- Footer Top -->
+    <div class="footer-top section ">
+        <div class="container">
+            <div class="row">
+                {{--            <div class="col-lg-5 col-md-6 col-12">
 
-	<!-- Start Footer Area -->
-	<footer class="footer">
-		<!-- Footer Top -->
-		<div class="footer-top section ">
-			<div class="container">
-				<div class="row">
-					<!-- <div class="col-lg-5 col-md-6 col-12">
-						
-						<div class="single-footer about">
-							<div class="logo">
-								<a href="index.html"><img src="{{asset('backend/img/logo2.png')}}" alt="#"></a>
-							</div>
-							@php
-								$settings=DB::table('settings')->get();
-							@endphp
-							<p class="text">@foreach($settings as $data) {{$data->short_des}} @endforeach</p>
-							<p class="call">Got Question? Call us 24/7<span><a href="tel:123456789">@foreach($settings as $data) {{$data->phone}} @endforeach</a></span></p>
-						</div>
-					
-					</div> -->
-					
+                                        <div class="single-footer about">
+                                            <div class="logo">
+                                                <a href="index.html"><img src="{{asset('backend/img/logo2.png')}}" alt="#"></a>
+                                            </div>
+                                            @php
+                                $settings=DB::table('settings')->get();
+                            @endphp
+                                <p class="text">@foreach($settings as $data) {{$data->short_des}} @endforeach</p>
+                                            <p class="call">Got Question? Call us 24/7<span><a href="tel:123456789">@foreach($settings as $data) {{$data->phone}} @endforeach</a></span></p>
+                                        </div>
 
-					<div class="col-lg-4 col-md-6 col-12">
-						<!-- Single Widget -->
-						<div class="single-footer links">
-							<h4>Information</h4>
-							<ul>
-								<li><a href="{{route('about-us')}}">About Us</a></li>
-								<li><a href="{{route('fqa')}}">Faq</a></li>
-								<li><a href="{{route('terms-conditions')}}">Terms & Conditions</a></li>
-								<li><a href="{{route('contact')}}">Contact Us</a></li>
-							</ul>
-						</div>
-						<!-- End Single Widget -->
-					</div>
-					<div class="col-lg-4 col-md-6 col-12">
-						<!-- Single Widget -->
-						<div class="single-footer links">
-							<h4>Customer Service</h4>
-							<ul>
-								<li><a href="{{route('payment-method')}}">Payment Methods</a></li>
-								<li><a href="{{route('exchange-refund')}}">Exchange & Refunds</a></li>
-								<li><a href="#">Shipping</a></li>
-								<li><a href="{{route('privacy-policy')}}">Privacy Policy</a></li>
-							</ul>
-						</div>
-						<!-- End Single Widget -->
-					</div>
-					<div class="col-lg-4 col-md-6 col-12">
-						<!-- Single Widget -->
-						<div class="single-footer social">
-							<h4>Get In Tuch</h4>
-							<!-- Single Widget -->
-							<div class="contact">
-								<ul>
-									<li>@foreach($settings as $data) {{$data->address}} @endforeach</li>
-									<li><a href="mailto: arshadgsh@gmail.com">arshadgsh@gmail.com</a></li>
-									<li><a href="tel:03023945180">0302 3945180</a></li>
-								</ul>
-							</div>
-							<!-- End Single Widget -->
-							<div class="sharethis-inline-follow-buttons"></div>
-						</div>
-						<!-- End Single Widget -->
-					</div>
-					<!-- <div class="col-lg-3 col-md-6 col-12">
-					<div class="single-footer newsletter-footer">
-					<h4>Newsletter</h4>
-					<p class="text-white pb-3">
-					You may unsubscribe at any moment. For that purpose, please find our contact info in the legal notice.
-					</p>
-					<div class="shop-newsletter section p-0">
-  
-						<div class="inner-top">
-				
-								
-									<div class="inner">
-									
-										<form action="{{route('subscribe')}}" method="post" class="newsletter-inner d-flex">
-											@csrf
-											<input name="email" placeholder="Your email address" required="" type="email">
-											<button class="btn" type="submit"><i class="ti-arrow-right"></i>
-                                            </button>
-										</form>
-									</div>
-								
-							
-						</div>
-   
-						</div>
-												
-						</div>
-						
-					</div> -->
-				</div>
-			</div>
-		</div>
-		<!-- End Footer Top -->
-	
-	</footer>
-	<div class="copyright">
-			<div class="container">
-				<div class="inner pt-4 pb-4">
-					<div class="row">
-						<div class="col-lg-12 col-12">
-							<div class="d-flex justify-content-center align-items-center">
-								<p>Copyright © {{date('Y')}} <a href="https://github.com/Prajwal100" target="_blank">Prajwal Rai</a>  -  All Rights Reserved.</p>
-							</div>
-						</div>
-						<!-- <div class="col-lg-4 col-12">
-							<div class="">
-							<ul class="d-flex justify-content-around">
-                            <li><a href="#"><img src="{{asset('backend/img/1.png')}}" alt="" width="50"/></a></li>
-                            <li><a href="#"><img src="{{asset('backend/img/2.png')}}" alt="" width="50"/></a></li>
-                            <li><a href="#"><img src="{{asset('backend/img/3.png')}}" alt="" width="50"/></a></li>
-                            <li><a href="#"><img src="{{asset('backend/img/4.png')}}" alt="" width="50"/></a></li>
-                            <li><a href="#"><img src="{{asset('backend/img/5.png')}}" alt="" width="50"/></a></li>
-                            <li><a href="#"><img src="{{asset('backend/img/6.png')}}" alt="" width="50"/></a></li>
+                                    </div> --}}
+                <div class="col-lg-4 col-md-6 col-12">
+                    <!-- Single Widget -->
+                    <div class="single-footer links">
+                        <h4>Information</h4>
+                        <ul>
+                            <li><a href="{{route('about-us')}}">About Us</a></li>
+                            <li><a href="{{route('fqa')}}">Faq</a></li>
+                            <li><a href="{{route('terms-conditions')}}">Terms & Conditions</a></li>
+                            <li><a href="{{route('contact')}}">Contact Us</a></li>
                         </ul>
-							
-							</div>
-						</div> -->
-					</div>
-				</div>
-			</div>
-		</div>
-	<!-- /End Footer Area -->
- 
-	<!-- Jquery -->
-    <script src="{{asset('frontend/js/jquery.min.js')}}"></script>
-    <script src="{{asset('frontend/js/jquery-migrate-3.0.0.js')}}"></script>
-	<script src="{{asset('frontend/js/jquery-ui.min.js')}}"></script>
-	<!-- Popper JS -->
-	<script src="{{asset('frontend/js/popper.min.js')}}"></script>
-	<!-- Bootstrap JS -->
-	<script src="{{asset('frontend/js/bootstrap.min.js')}}"></script>
-	<!-- Color JS -->
-	<script src="{{asset('frontend/js/colors.js')}}"></script>
-	<!-- Slicknav JS -->
-	<script src="{{asset('frontend/js/slicknav.min.js')}}"></script>
-	<!-- Owl Carousel JS -->
-	<script src="{{asset('frontend/js/owl-carousel.js')}}"></script>
-	<!-- Magnific Popup JS -->
-	<script src="{{asset('frontend/js/magnific-popup.js')}}"></script>
-	<!-- Waypoints JS -->
-	<script src="{{asset('frontend/js/waypoints.min.js')}}"></script>
-	<!-- Countdown JS -->
-	<script src="{{asset('frontend/js/finalcountdown.min.js')}}"></script>
-	<!-- Nice Select JS -->
-	<script src="{{asset('frontend/js/nicesellect.js')}}"></script>
-	<!-- Flex Slider JS -->
-	<script src="{{asset('frontend/js/flex-slider.js')}}"></script>
-	<!-- ScrollUp JS -->
-	<script src="{{asset('frontend/js/scrollup.js')}}"></script>
-	<!-- Onepage Nav JS -->
-	<script src="{{asset('frontend/js/onepage-nav.min.js')}}"></script>
-	{{-- Isotope --}}
-	<script src="{{asset('frontend/js/isotope/isotope.pkgd.min.js')}}"></script>
-	<!-- Easing JS -->
-	<script src="{{asset('frontend/js/easing.js')}}"></script>
+                    </div>
+                    <!-- End Single Widget -->
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    <!-- Single Widget -->
+                    <div class="single-footer links">
+                        <h4>Customer Service</h4>
+                        <ul>
+                            <li><a href="{{route('payment-method')}}">Payment Methods</a></li>
+                            <li><a href="{{route('exchange-refund')}}">Exchange & Refunds</a></li>
+                            <li><a href="#">Shipping</a></li>
+                            <li><a href="{{route('privacy-policy')}}">Privacy Policy</a></li>
+                        </ul>
+                    </div>
+                    <!-- End Single Widget -->
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    <!-- Single Widget -->
+                    <div class="single-footer social">
+                        <h4>Get In Tuch</h4>
+                        <!-- Single Widget -->
+                        <div class="contact">
+                            <ul>
+                                <li>@foreach($settings as $data) {{$data->address}} @endforeach</li>
+                                <li><a href="mailto: arshadgsh@gmail.com">arshadgsh@gmail.com</a></li>
+                                <li><a href="tel:03023945180">0302 3945180</a></li>
+                            </ul>
+                        </div>
+                        <!-- End Single Widget -->
+                        <div class="sharethis-inline-follow-buttons"></div>
+                    </div>
+                    <!-- End Single Widget -->
+                </div>
+                {{--            <div class="col-lg-3 col-md-6 col-12">
+                                    <div class="single-footer newsletter-footer">
+                                    <h4>Newsletter</h4>
+                                    <p class="text-white pb-3">
+                                    You may unsubscribe at any moment. For that purpose, please find our contact info in the legal notice.
+                                    </p>
+                                    <div class="shop-newsletter section p-0">
 
-	<!-- Active JS -->
-	<script src="{{asset('frontend/js/active.js')}}"></script>
-
-	
-	@stack('scripts')
-	<script>
-		setTimeout(function(){
-		  $('.alert').slideUp();
-		},5000);
-		$(function() {
-		// ------------------------------------------------------- //
-		// Multi Level dropdowns
-		// ------------------------------------------------------ //
-			$("ul.dropdown-menu [data-toggle='dropdown']").on("click", function(event) {
-				event.preventDefault();
-				event.stopPropagation();
-
-				$(this).siblings().toggleClass("show");
+                                        <div class="inner-top">
 
 
-				if (!$(this).next().hasClass('show')) {
-				$(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
-				}
-				$(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
-				$('.dropdown-submenu .show').removeClass("show");
-				});
+                                                    <div class="inner">
 
-			});
-		});
-	  </script>
+                                                        <form action="{{route('subscribe')}}" method="post" class="newsletter-inner d-flex">
+                                                            @csrf
+                                <input name="email" placeholder="Your email address" required="" type="email">
+                                <button class="btn" type="submit"><i class="ti-arrow-right"></i>
+                                </button>
+                            </form>
+                        </div>
+
+
+                </div>
+
+                </div>
+
+                </div>
+
+                </div> --}}
+            </div>
+        </div>
+    </div>
+    <!-- End Footer Top -->
+
+</footer>
+<div class="copyright">
+    <div class="container">
+        <div class="inner pt-4 pb-4">
+            <div class="row">
+                <div class="col-lg-12 col-12">
+                    <div class="d-flex justify-content-center align-items-center">
+                        {{--								<p>Copyright © {{date('Y')}} <a href="https://github.com/Prajwal100" target="_blank">Prajwal Rai</a>  -  All Rights Reserved.</p>--}}
+                        <p>Copyright © {{date('Y')}} <a href="{{url('/')}}" target="_blank">Ghousia</a> - All Rights
+                            Reserved.</p>
+                    </div>
+                </div>
+                {{--						<div class="col-lg-4 col-12">
+                                            <div class="">
+                                            <ul class="d-flex justify-content-around">
+                                            <li><a href="#"><img src="{{asset('backend/img/1.png')}}" alt="" width="50"/></a></li>
+                                            <li><a href="#"><img src="{{asset('backend/img/2.png')}}" alt="" width="50"/></a></li>
+                                            <li><a href="#"><img src="{{asset('backend/img/3.png')}}" alt="" width="50"/></a></li>
+                                            <li><a href="#"><img src="{{asset('backend/img/4.png')}}" alt="" width="50"/></a></li>
+                                            <li><a href="#"><img src="{{asset('backend/img/5.png')}}" alt="" width="50"/></a></li>
+                                            <li><a href="#"><img src="{{asset('backend/img/6.png')}}" alt="" width="50"/></a></li>
+                                        </ul>
+
+                                            </div>
+                                        </div> --}}
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /End Footer Area -->
+
+<!-- Jquery -->
+<script src="{{asset('frontend/js/jquery.min.js')}}"></script>
+<script src="{{asset('frontend/js/jquery-migrate-3.0.0.js')}}"></script>
+<script src="{{asset('frontend/js/jquery-ui.min.js')}}"></script>
+<!-- Popper JS -->
+<script src="{{asset('frontend/js/popper.min.js')}}"></script>
+<!-- Bootstrap JS -->
+<script src="{{asset('frontend/js/bootstrap.min.js')}}"></script>
+<!-- Color JS -->
+<script src="{{asset('frontend/js/colors.js')}}"></script>
+<!-- Slicknav JS -->
+<script src="{{asset('frontend/js/slicknav.min.js')}}"></script>
+<!-- Owl Carousel JS -->
+<script src="{{asset('frontend/js/owl-carousel.js')}}"></script>
+<!-- Magnific Popup JS -->
+<script src="{{asset('frontend/js/magnific-popup.js')}}"></script>
+<!-- Waypoints JS -->
+<script src="{{asset('frontend/js/waypoints.min.js')}}"></script>
+<!-- Countdown JS -->
+<script src="{{asset('frontend/js/finalcountdown.min.js')}}"></script>
+<!-- Nice Select JS -->
+<script src="{{asset('frontend/js/nicesellect.js')}}"></script>
+<!-- Flex Slider JS -->
+<script src="{{asset('frontend/js/flex-slider.js')}}"></script>
+<!-- ScrollUp JS -->
+<script src="{{asset('frontend/js/scrollup.js')}}"></script>
+<!-- Onepage Nav JS -->
+<script src="{{asset('frontend/js/onepage-nav.min.js')}}"></script>
+{{-- Isotope --}}
+<script src="{{asset('frontend/js/isotope/isotope.pkgd.min.js')}}"></script>
+<!-- Easing JS -->
+<script src="{{asset('frontend/js/easing.js')}}"></script>
+
+<!-- Active JS -->
+<script src="{{asset('frontend/js/active.js')}}"></script>
+
+
+@stack('scripts')
+<script>
+    setTimeout(function () {
+        $('.alert').slideUp();
+    }, 5000);
+    $(function () {
+        // ------------------------------------------------------- //
+        // Multi Level dropdowns
+        // ------------------------------------------------------ //
+        $("ul.dropdown-menu [data-toggle='dropdown']").on("click", function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            $(this).siblings().toggleClass("show");
+            if (!$(this).next().hasClass('show')) {
+                $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+            }
+            $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+                $('.dropdown-submenu .show').removeClass("show");
+            });
+        });
+    });
+</script>
